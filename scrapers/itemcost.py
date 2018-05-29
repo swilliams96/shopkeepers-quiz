8ecd9e70dfd5a1e6bd797914727b83a880fb1900
Sam W <swilliams96@outlook.com>
2018-05-29 14:06:15 +0100

    Initial commit - basic working version

---
+++ scrapers/itemcost.py
@@ -0,0 +1,108 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import time
+import random
+
+class Item:
+    def __init__(self, name, cost, url):
+        self.name = name
+        self.cost = cost
+        self.url = url
+
+    def getWrongAnswers(self):
+        result = '['
+
+        if self.cost <= 25:
+            return '[]'
+        if self.cost > 25:
+            factor = 5
+            limit = 5
+        if self.cost > 100:
+            factor = 10
+            limit = 8
+        if self.cost > 400:
+            factor = 10
+            limit = 20
+            if self.cost % 25 == 0:
+                factor = 25
+                limit = 10
+        if self.cost > 800:
+            factor = 25
+            limit = 30
+        if self.cost > 1400:
+            factor = 25
+            limit = 50
+        if self.cost > 2000:
+            factor = 100
+            limit = 15
+
+        count = random.randint(4, 8)
+        for r in range(0, count):
+            x = 0
+            while x == 0 and result.find(str(self.cost + x*factor)) != -1:
+                x = random.randint(-limit, limit)
+            result += str(self.cost + x*factor) + ', '
+
+        return result[:-2] + ']'
+
+
+itemdictionary = {}
+
+print('Getting page data... https://dota2.gamepedia.com/Items')
+page = requests.get('https://dota2.gamepedia.com/Items')
+soup = BeautifulSoup(page.content, 'html.parser')
+
+print('Parsing HTML...')
+itemlists = soup.find_all('div', attrs={'class': 'itemlist'})
+if len(itemlists) == 0:
+    print('Parse failed - could not find any itemlist div elements')
+    exit()
+
+for itemlist in itemlists:
+    if itemlist.find('div') is None:
+        continue
+    items = itemlist.find_all('div')
+    for item in items:
+        if item.find('a'):
+            link = item.find('a')
+            text = link['title']
+            text = text.replace(' (Agility)', '')           # For Power Treads
+            text = text.replace(' (Strength)', '')          # For Power Treads
+            text = text.replace(' (Intelligence)', '')      # For Power Treads
+            split = str.split(text, '(', 1)
+
+            if len(split) <= 1:                 # Item has no cost/is not purchasable so skip
+                continue
+            if split[0].find('Aegis') != -1:    # Items after the aegis are event/temporary items
+                print('\nReached Aegis of the Immortal - end of purchasable items...')
+                exit()
+
+            i_name = split[0][:-1]
+            i_cost = int(split[1][:-1])
+            i_url = link['href']
+            itemdictionary[i_name] = Item(i_name, i_cost, i_url)
+print(len(itemdictionary), 'items found\n')
+
+print('Getting item image URLs...\n\n')
+json = '    {\n      '
+for name, item in itemdictionary.items():
+    page = requests.get('https://dota2.gamepedia.com' + str(item.url))
+    soup = BeautifulSoup(page.content, 'html.parser')
+    img = soup.find('td', attrs={'id': 'itemmainimage'}).find('img')
+    if img is not None:
+        img_url = img['src'].split('?')[0]
+        print(name, '|', item.cost, '|', img_url)
+        json += '"question": "How much does {name} cost?",\n      '.format(name=name)
+        json += '"image": "{url}",\n      '.format(url=img_url)
+        json += '"type": "gold",\n      "correct": "{cost}",\n      '.format(cost=item.cost)
+        json += '"wrong": {wronganswers}\n    '.format(wronganswers=item.getWrongAnswers())
+        json += '}, {'
+    time.sleep(0.33)
+
+full_json = '{\n  "questions": [\n' + json[:-3] + '\n  ]\n}'
+print('JSON:\n\n' + full_json)
+
+f = open('output/itemcost-' + str(datetime.now().date().isoformat()) + '.json', 'w')
+f.write(full_json)
+
---
+++ src/App.js
@@ -0,0 +1,293 @@
+import React, { Component } from 'react';
+import './reset.css';
+import './loader.css';
+import './App.css';
+
+import QuestionData from './QuestionData.json';
+
+import mana from './img/mana.png';
+import cooldown from './img/cooldown.png';
+import gold from './img/gold.png';
+
+const questions = QuestionData["questions"];
+const defaultTime = 15;
+
+function shuffle(array) {
+	var m = array.length, t, i;
+	while (m) {																// While there remain elements to shuffle…
+		i = Math.floor(Math.random() * m--);		// Pick a remaining element…
+		t = array[m];														// And swap it...
+		array[m] = array[i];										// ...with the...
+		array[i] = t;														// ...current element.
+	}
+	return array;
+}
+
+
+
+/* * * * * * * * * * * * * * * * */
+/* * * * REACT COMPONENTS  * * * */
+/* * * * * * * * * * * * * * * * */
+
+class Answer extends Component {
+	getIcon(type) {
+		if (type === "mana") {
+			return <img src={mana} className={"icon"} alt="Mana Cost"/>
+		} else if (type === "cooldown") {
+			return <img src={cooldown} className={"icon"} alt="Cooldown"/>
+		} else if (type === "gold") {
+			return <img src={gold} className={"icon"} alt="Gold Cost"/>
+		}
+
+		return;
+	}
+
+	render() {
+		const icon = (this.props.type !== "" ? this.getIcon(this.props.type) : "");
+		return (
+			<div className="Answer">
+				<a onClick={() => this.props.handleClick(this.props.value)} className={this.props.className || (this.props.active ? "active" : "")}>
+					<p>{icon}{this.props.value}</p>
+				</a>
+			</div>
+		);
+	}
+}
+
+
+class AnswerContainer extends Component {
+	renderAnswer(answer, key) {
+		let className = (answer === this.props.selected ? "selected " : "");
+		if (this.props.correctAnswer)
+			className += (answer === this.props.correctAnswer ? "correct" : "incorrect");
+
+		return (
+			<Answer value={answer}
+							key={key}
+							handleClick={this.props.handleClick}
+							active={this.props.active}
+							type={this.props.data["type"]}
+							className={className} />
+		);
+	}
+
+	getAnswers() {
+		let options = this.props.data["answers"], answers = [];
+		for (let i = 0; i < options.length; i++) {
+			answers.push(this.renderAnswer(options[i], i));
+		}
+		return answers;
+	}
+
+	render() {
+		return (
+			<div className="AnswerContainer">
+				{this.getAnswers()}
+			</div>
+		);
+	}
+}
+
+
+class Result extends Component {
+	getTime(secs = 0) {
+		let mins = 0;
+		secs = Math.max(0, Math.round(secs));
+		while (true) {
+			if (secs < 60) break;
+			secs = secs - 60;
+			mins++;
+		}
+		if (secs < 10) secs = '0' + secs;
+		return mins + ':' + secs;
+	}
+
+	render() {
+		if (this.props.correct === null) {
+			return (
+				<div className="Result">
+					<p style={{paddingTop: 20}}>Time Remaining</p>
+					<span className="large-text">{this.getTime(this.props.countdown)}</span>
+				</div>
+			);
+		} else {
+			return (
+				<div className="Result">
+					<span className="large-text">{this.props.correct ? "+100" : "+0"} pts</span>
+					<p>Next question in {this.getTime(this.props.countdown)}...</p>
+				</div>
+			);
+		}
+	}
+}
+
+
+class Question extends Component {
+	render() {
+		const image = (this.props.data["image"] ? <img src={this.props.data["image"]} className={"image"} alt=""/> : '');
+		return (
+			<div className="Question">
+				<h1>{this.props.data["question"]}</h1>
+				{image}
+			</div>
+		);
+	}
+}
+
+
+function Loader() {
+	return (
+		<div className="Loader">
+			<div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+			<p>Loading...</p>
+		</div>
+	);
+}
+
+
+class PanelBody extends Component {
+	render() {
+		if (this.props.question) {
+			return (
+				<div className="PanelBody">
+					<Question data={this.props.question}/>
+					<AnswerContainer data={this.props.question}
+													 active={this.props.active}
+													 handleClick={this.props.handleClick}
+													 selected={this.props.selected}
+													 correctAnswer={this.props.correct !== null ? this.props.question["correct"] : ""}/>
+					<Result correct={this.props.correct} countdown={this.props.countdown}/>
+				</div>
+			);
+		} else {
+			return <Loader/>;
+		}
+	}
+}
+
+
+class PanelHeader extends Component {
+	render() {
+		return (
+			<div className="PanelHeader">
+				<b className="left-align">DotA 2 Trivia</b>
+				<span className="right-align score">{this.props.score} pts</span>
+			</div>
+		);
+	}
+}
+
+
+class Panel extends Component {
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+		this.state = {
+			score: 0,
+			question: null,
+			countdown: null,
+			active: true,
+			correct: null,
+			selected: null
+		}
+	}
+
+	componentDidMount() {
+		this.getNewQuestion();
+		this.timerID = setInterval(
+			() => this.tick(),
+			1000
+		);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+
+	getNewQuestion() {
+		console.log("Getting new question...");
+		let question = questions[Math.floor(Math.random() * questions.length)];
+
+		let options = [], fillers = question["wrong"].slice();
+		options.push(question["correct"]);											// Add correct answer to the options
+		for (let i = 0; i < 3; i++) {														// Loop through wrong answers and pick 3 at random
+			let x = Math.floor(Math.random() * fillers.length);
+			options.push(fillers[x]);
+			fillers.splice(x, 1);
+		}
+
+		question["answers"] = shuffle(options);									// Shuffle the final options and add them to the array
+		let countdown = (question["time"] || defaultTime);			// Get the countdown time for this question
+
+		this.setState({																					// Start the new question
+			question: question,
+			selected: null,
+			countdown: countdown,
+			active: true,
+			correct: null
+		});
+	}
+
+	handleClick(answer) {
+		if (this.state.active) {
+			this.setState({
+				active: false,
+				selected: answer
+			});
+		}
+	}
+
+	checkAnswer() {
+		if (this.state.selected === this.state.question["correct"]) {
+			this.setState((prevState) => ({
+				correct: true,
+				score: prevState.score + 100,
+			}));
+		} else {
+			this.setState({correct: false});
+		}
+	}
+
+	tick() {
+		if (this.state.countdown <= 0) {
+			if (this.state.correct === null) {
+				this.checkAnswer();
+				this.setState({
+					active: false,
+					countdown: 6
+				});
+			} else {
+				this.getNewQuestion();
+			}
+		}
+
+		this.setState((prevState) => ({countdown: prevState.countdown - 1}));
+	}
+
+	render() {
+		return (
+			<div className="Panel">
+				<PanelHeader score={this.state.score}/>
+				<PanelBody question={this.state.question}
+									 selected={this.state.selected}
+									 active={this.state.active}
+									 correct={this.state.correct}
+									 handleClick={this.handleClick}
+									 countdown={this.state.countdown}/>
+			</div>
+		);
+	}
+}
+
+
+class App extends Component {
+  render() {
+    return (
+    	<div className="App">
+				<Panel/>
+			</div>
+		);
+  }
+}
+
+export default App;
