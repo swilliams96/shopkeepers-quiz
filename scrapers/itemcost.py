53c4f176fc2670ff00342b27d60e59eb28b97244
Sam W <swilliams96@outlook.com>
2018-06-18 23:08:31 +0100

    Fixed issues with itemcost scraper

--- scrapers/itemcost.py
+++ scrapers/itemcost.py
@@ -37,13 +37,14 @@ class Item:
             factor = 100
             limit = 15
 
-        count = random.randint(4, 8)
+        count = random.randint(5, 8)
         for r in range(0, count):
-            x = 0
-            while x == 0 and result.find(str(self.cost + x*factor)) != -1:
+            valid = False
+            while not valid:
                 x = random.randint(-limit, limit)
-            result += str(self.cost + x*factor) + ', '
-
+                if x != 0 and result.find(str(self.cost + x*factor)) == -1:
+                    valid = True
+                    result += str(self.cost + x*factor) + ', '
         return result[:-2] + ']'
 
 
@@ -58,7 +59,9 @@ itemlists = soup.find_all('div', attrs={'class': 'itemlist'})
 if len(itemlists) == 0:
     print('Parse failed - could not find any itemlist div elements')
     exit()
+print(len(itemlists), 'itemlists found')
 
+aegisReached = False
 for itemlist in itemlists:
     if itemlist.find('div') is None:
         continue
@@ -70,22 +73,26 @@ for itemlist in itemlists:
             text = text.replace(' (Agility)', '')           # For Power Treads
             text = text.replace(' (Strength)', '')          # For Power Treads
             text = text.replace(' (Intelligence)', '')      # For Power Treads
-            split = str.split(text, '(', 1)
 
+            if text.lower().find('aegis') != -1:  # Items after the aegis are event/temporary items
+                print('\nReached Aegis of the Immortal - end of purchasable items...')
+                aegisReached = True
+                break
+
+            split = str.split(text, '(', 1)
             if len(split) <= 1:                 # Item has no cost/is not purchasable so skip
                 continue
-            if split[0].find('Aegis') != -1:    # Items after the aegis are event/temporary items
-                print('\nReached Aegis of the Immortal - end of purchasable items...')
-                exit()
 
             i_name = split[0][:-1]
             i_cost = int(split[1][:-1])
             i_url = link['href']
             itemdictionary[i_name] = Item(i_name, i_cost, i_url)
+    if aegisReached:                            # Don't proceed to the next itemlist if we've reached the aegis
+        break
 print(len(itemdictionary), 'items found\n')
 
-print('Getting item image URLs...\n\n')
-json = '    {\n      '
+print('Getting high quality item image URLs...\n\n')
+json = '    {'
 for name, item in itemdictionary.items():
     page = requests.get('https://dota2.gamepedia.com' + str(item.url))
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -93,6 +100,7 @@ for name, item in itemdictionary.items():
     if img is not None:
         img_url = img['src'].split('?')[0]
         print(name, '|', item.cost, '|', img_url)
+        json += '\n      '
         json += '"question": "How much does {name} cost?",\n      '.format(name=name)
         json += '"image": "{url}",\n      '.format(url=img_url)
         json += '"type": "gold",\n      "correct": "{cost}",\n      '.format(cost=item.cost)
